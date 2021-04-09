e460ea99450d7fba7e93005c96a42b6e9fff3783
Glitch (synx-v12-guard-bot-altyapi) <none>
2021-04-09 18:10:09 +0000

    🚨🗾 Checkpoint ./ayarlar.json:29072165/36 ./komutlar/yardım.js:29072165/34 ./bot.js:29072165/31

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on("ready", async () => {
     client.appInfo = await client.fetchApplication();
   }, 600);
 
-  client.user.setActivity(`T E N R A`, { type: "WATCHING" });
+  client.user.setActivity(`V A M P İ R E `, { type: "WATCHING" });
 
   console.log("Bot Aktif!");
 });
