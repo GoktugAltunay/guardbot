4cc5b9f301bc95158208674e679606c5fed111ac
Glitch (synx-v12-guard-bot-altyapi) <none>
2021-04-09 23:06:20 +0000

    😇🏪 Checkpoint ./komutlar/yardım.js:29072165/170

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -9,22 +9,22 @@ exports.run = async(client, message, args) => {
 
              .setColor('#fff000')
              .addField(`T E N R A Guard Yardım Komutları`, `
-             • <a:yldzlar:826880850225922049> ${prefix}antiraid aç
-             • <a:yldzlar:826880850225922049> ${prefix}capsengel aç
-             • <a:yldzlar:826880850225922049> ${prefix}emojikoruma aç
-             • <a:yldzlar:826880850225922049> ${prefix}everengel aç
-             • <a:yldzlar:826880850225922049> ${prefix}kanalkoruma aç
-             • <a:yldzlar:826880850225922049> ${prefix}küfürengel aç
-             • <a:yldzlar:826880850225922049> ${prefix}modlog #kanal
-             • <a:yldzlar:826880850225922049> ${prefix}reklamengel aç
-             • <a:yldzlar:826880850225922049> ${prefix}rolkoruma aç
-             • <a:yldzlar:826880850225922049> ${prefix}sohbet-aç
-             • <a:yldzlar:826880850225922049> ${prefix}sohbet-kapat
-             • <a:yldzlar:826880850225922049> ${prefix}sil-üye @üye miktar
-             • <a:yldzlar:826880850225922049> ${prefix}sil miktar
-             • <a:yldzlar:826880850225922049> ${prefix}yavaşmod süre
-             • <a:yldzlar:826880850225922049> ${prefix}ban @üye sebep
-             • <a:yldzlar:826880850225922049> ${prefix}kick @üye sebep             
+             • <a826880850225922049> ${prefix}antiraid aç
+             • <a826880850225922049> ${prefix}capsengel aç
+             • <a826880850225922049> ${prefix}emojikoruma aç
+             • <a826880850225922049> ${prefix}everengel aç
+             • <a826880850225922049> ${prefix}kanalkoruma aç
+             • <a826880850225922049> ${prefix}küfürengel aç
+             • <a826880850225922049> ${prefix}modlog #kanal
+             • <a826880850225922049> ${prefix}reklamengel aç
+             • <a826880850225922049> ${prefix}rolkoruma aç
+             • <a826880850225922049> ${prefix}sohbet-aç
+             • <a826880850225922049> ${prefix}sohbet-kapat
+             • <a826880850225922049> ${prefix}sil-üye @üye miktar
+             • <a826880850225922049> ${prefix}sil miktar
+             • <a826880850225922049> ${prefix}yavaşmod süre
+             • <a826880850225922049> ${prefix}ban @üye sebep
+             • <a826880850225922049> ${prefix}kick @üye sebep             
              `)
              .setFooter(`${message.author.username} Tarafından istendi.`, message.author.avatarURL())
             
