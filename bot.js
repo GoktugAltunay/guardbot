5769fd32e172e10248fcb97f99f82a7deb534804
Glitch (synx-v12-guard-bot-altyapi) <none>
2021-04-17 15:34:58 +0000

    🚩🏠 Checkpoint ./bot.js:29072165/30

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on("ready", async () => {
     client.appInfo = await client.fetchApplication();
   }, 600);
 
-  client.user.setActivity(`Wio ❤️ Clark`, { type: "WATCHING" });
+  client.user.setActivity(`Vampire Code`, { type: "WATCHING" });
 
   console.log("Bot Aktif!");
 });
